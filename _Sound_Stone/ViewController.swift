338cd7ce4a3ef75da28da9d53ddc01dcf83cb1ec
Yusuke Watanabe <g16tk018@Yusuke-no-Mac-Pro.local>
2016-10-18 17:48:57 +0900

    Initial Commit

---
+++ _Sound_Stone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  _Sound_Stone
+//
+//  Created by Yusuke Watanabe on 2016/10/18.
+//  Copyright © 2016年 Yusuke Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
